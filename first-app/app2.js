2c82b76a1c781c8887b2a1a14142e79b3d650e25
mohamed085 <mohamed085555@gmail.com>
2021-04-15 14:25:38 +0200

    v-on

--- first-app/app2.js
+++ first-app/app2.js
@@ -2,8 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
+            name: ''
         };
     },
+    methods: {
+        add(num) {
+            this.counter = this.counter + num;
+        },
+        reduce(num) {
+            this.counter = this.counter - num;
+        },
+        setName() {
+            this.name = event.target.value;
+        }
+    }
 });
 
 app.mount('#events');
