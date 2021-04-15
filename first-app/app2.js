fd52001d0dab4368ec2815c027801c8e269dcbb3
mohamed085 <mohamed085555@gmail.com>
2021-04-15 14:31:09 +0200

    v-on

--- first-app/app2.js
+++ first-app/app2.js
@@ -6,6 +6,9 @@ const app = Vue.createApp({
         };
     },
     methods: {
+        submitForm(){
+          alert("Submitted");
+        },
         add(num) {
             this.counter = this.counter + num;
         },
