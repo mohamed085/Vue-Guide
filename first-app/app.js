5a520a112154aa16a3a40cfc987d7ff34c500e8f
mohamed085 <mohamed085555@gmail.com>
2021-04-15 14:12:00 +0200

    this

--- first-app/app.js
+++ first-app/app.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             course: "Welcome my friend",
-            googleLink: "https://www.google.com/"
+            googleLink: "https://www.google.com/",
+            courseGoalA : "FinishA",
+            courseGoalB : "FinishB"
         };
     },
     methods: {
@@ -13,7 +15,16 @@ const app = Vue.createApp({
             } else {
                 return 'Not Learn Vue';
             }
-        }
+        },
+        outputGoalA(){
+            const r = Math.random();
+            if (r < .5){
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        },
+
     }
 });
 
