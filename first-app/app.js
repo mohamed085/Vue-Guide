b1657b3463d55b336df8d547649a12d3b83725ab
mohamed085 <mohamed085555@gmail.com>
2021-04-15 14:09:52 +0200

    v-bind & methods

--- first-app/app.js
+++ first-app/app.js
@@ -2,8 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             course: "Welcome my friend",
+            googleLink: "https://www.google.com/"
         };
     },
+    methods: {
+        outputGoal(){
+            const r = Math.random();
+            if (r < .5){
+                return 'Learn Vue';
+            } else {
+                return 'Not Learn Vue';
+            }
+        }
+    }
 });
 
 app.mount('#user-goal');
