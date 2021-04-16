9b47907094f015dfa41371af521e6fdfb1550e95
mohamed085 <mohamed085555@gmail.com>
2021-04-16 14:52:14 +0200

    add main layout

---
+++ vue-resources-app/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <TheHeader
+      title="RememberMe"
+  ></TheHeader>
+  <StoredResources
+      :resources="storedResources"
+  ></StoredResources>
+</template>
+
+<script>
+import StoredResources from "./components/learning-resources/StoredResources";
+import TheHeader from "./components/layouts/TheHeader";
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vue-resources-app/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,54 @@
+<template>
+  <li>
+    <BaseCard>
+      <header>
+        <h3>{{ title }}</h3>
+        <base-button mode="flat">Delete</base-button>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
+    </BaseCard>
+  </li>
+</template>
+
+<script>
+import BaseCard from "../UI/BaseCard";
+export default {
+  components: {BaseCard},
+  props: ['title', 'description', 'link'],
+};
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
