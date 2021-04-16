6633ed45accb32711b94024ef5637651cf9c302c
mohamed085 <mohamed085555@gmail.com>
2021-04-16 13:03:43 +0200

    provide & inject

--- vue-topic-app/src/App.vue
+++ vue-topic-app/src/App.vue
@@ -32,6 +32,25 @@ export default {
       activeTopic: null,
     };
   },
+  provide: {
+    topics: [
+      {
+        id: 'basics',
+        title: 'The Basics',
+        description: 'Core Vue basics you have to know',
+        fullText:
+            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+      },
+      {
+        id: 'components',
+        title: 'Components',
+        description:
+            'Components are a core concept for building Vue UIs and apps',
+        fullText:
+            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+      },
+    ]
+  },
   methods: {
     activateTopic(topicId) {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
