34afdc6977eeea4c952eda6b427ffeb181c544a2
mohamed085 <mohamed085555@gmail.com>
2021-04-16 13:06:16 +0200

    another way to solve duplicate in data provide

--- vue-topic-app/src/App.vue
+++ vue-topic-app/src/App.vue
@@ -32,24 +32,10 @@ export default {
       activeTopic: null,
     };
   },
-  provide: {
-    topics: [
-      {
-        id: 'basics',
-        title: 'The Basics',
-        description: 'Core Vue basics you have to know',
-        fullText:
-            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
-      },
-      {
-        id: 'components',
-        title: 'Components',
-        description:
-            'Components are a core concept for building Vue UIs and apps',
-        fullText:
-            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
-      },
-    ]
+  provide() {
+    return {
+      topics: this.topics
+    }
   },
   methods: {
     activateTopic(topicId) {
