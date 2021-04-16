78169d7e8de408cbdbe98db4abfc2f1208ef6849
mohamed085 <mohamed085555@gmail.com>
2021-04-16 13:39:14 +0200

    Alert component

--- vue-more-on-component-app/src/App.vue
+++ vue-more-on-component-app/src/App.vue
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import TheHeader from './components/layout/TheHeader.vue';
+import TheHeader from './components/TheHeader.vue';
 // import BadgeList from './components/BadgeList.vue';
 // import UserInfo from './components/UserInfo.vue';
 // import CourseGoals from './components/CourseGoals.vue';
--- vue-more-on-component-app/src/components/ManageGoals.vue
+++ vue-more-on-component-app/src/components/ManageGoals.vue
@@ -1,14 +1,16 @@
 <template>
-  <h2>Manage Goals</h2>
-  <input type="text" ref="goal" />
-  <button @click="setGoal">Set Goal</button>
-  <teleport to="body">
-    <error-alert v-if="inputIsInvalid">
-      <h2>Input is invalid!</h2>
-      <p>Please enter at least a few characters...</p>
-      <button @click="confirmError">Okay</button>
-    </error-alert>
-  </teleport>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <teleport to="body">
+      <error-alert v-if="inputIsInvalid">
+        <h2>Input is invalid!</h2>
+        <p>Please enter at least a few characters...</p>
+        <button @click="confirmError">Okay</button>
+      </error-alert>
+    </teleport>
+  </div>
 </template>
 
 <script>
@@ -16,11 +18,11 @@ import ErrorAlert from './ErrorAlert.vue';
 
 export default {
   components: {
-    ErrorAlert,
+    ErrorAlert
   },
   data() {
     return {
-      inputIsInvalid: false,
+      inputIsInvalid: false
     };
   },
   methods: {
@@ -32,7 +34,7 @@ export default {
     },
     confirmError() {
       this.inputIsInvalid = false;
-    },
-  },
-};
+    }
+  }
+}
 </script>
