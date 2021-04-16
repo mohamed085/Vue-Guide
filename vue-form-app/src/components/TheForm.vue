5ccca0724a72d64159cdb534752c3e534f2b3e06
mohamed085 <mohamed085555@gmail.com>
2021-04-16 20:58:56 +0200

    Invalid input

--- vue-form-app/src/components/TheForm.vue
+++ vue-form-app/src/components/TheForm.vue
@@ -1,8 +1,18 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div
+        class="form-control"
+        :class="{invalid: userNameValidity === 'Invalid'}"
+    >
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+          id="user-name"
+          name="user-name"
+          type="text"
+          v-model.trim="userName"
+          @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'Invalid'">Please enter a valid name</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -77,7 +87,8 @@ export default {
       referrer: 'wom',
       interest: [],
       how: null,
-      confirm: false
+      confirm: false,
+      userNameValidity: 'pending'
     };
   },
   methods: {
@@ -101,6 +112,13 @@ export default {
       console.log(this.confirm);
       this.confirm = false;
     },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'Invalid';
+      } else {
+        this.userNameValidity = 'Valid';
+      }
+    }
   },
 };
 </script>
@@ -117,6 +135,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input{
+  border-color: red;
+}
+
+.form-control.invalid label{
+  color: red;
 }
 
 label {
