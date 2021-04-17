2df3a071c49df9d1db75f257af031967c0896498
mohamed085 <mohamed085555@gmail.com>
2021-04-17 17:37:50 +0200

    passing data with route params

--- vue-workshop-app/src/main.js
+++ vue-workshop-app/src/main.js
@@ -4,12 +4,14 @@ import { createRouter , createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from "./components/teams/TeamsList";
 import UsersList from "./components/users/UsersList";
+import TeamMembers from "./components/teams/TeamMembers";
 
 const app = createApp(App);
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/teams', component: TeamsList },
+        { path: '/teams/:teamId', component: TeamMembers },
         { path: '/users', component: UsersList }
     ],
     linkActiveClass: 'active'
