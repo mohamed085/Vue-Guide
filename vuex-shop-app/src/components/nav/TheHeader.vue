0f32a1cb3f1b2b7ba0543813b9c0640914620855
mohamed085 <mohamed085555@gmail.com>
2021-04-18 01:36:01 +0200

    modify project to vuex

--- vuex-shop-app/src/components/nav/TheHeader.vue
+++ vuex-shop-app/src/components/nav/TheHeader.vue
@@ -10,7 +10,7 @@
         </li>
         <li>
           <router-link to="/cart">Cart</router-link>
-          <base-badge mode="elegant">{{ cart.qty }}</base-badge>
+          <base-badge mode="elegant">{{ cartQuantity }}</base-badge>
         </li>
         <li v-if="isLoggedIn">
           <router-link to="/admin">Admin</router-link>
@@ -26,7 +26,22 @@
 
 <script>
 export default {
-  inject: ['isLoggedIn', 'login', 'logout', 'cart'],
+  computed: {
+    cartQuantity() {
+      return this.$store.getters['cart/quantity']
+    },
+    isLoggedIn() {
+      return this.$store.getters.isAuthenticated;
+    }
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('login');
+    },
+    logout() {
+      this.$store.dispatch('logout');
+    }
+  }
 };
 </script>
 
---
+++ vuex-shop-app/src/store/index.js
@@ -0,0 +1,39 @@
+import { createStore } from "vuex";
+
+import productsModule from "./modules/products";
+import cartModule from "./modules/cart";
+
+const store = createStore({
+    modules: {
+        prods: productsModule,
+        cart: cartModule
+    },
+    state() {
+        return {
+            isLoggedIn: false,
+        }
+    },
+    mutations: {
+        login(state) {
+            state.isLoggedIn = true;
+        },
+        logout(state) {
+            state.isLoggedIn = false;
+        },
+    },
+    actions: {
+        login(context) {
+            context.commit('login');
+        },
+        logout(context) {
+            context.commit('logout');
+        }
+    },
+    getters: {
+        isAuthenticated(state) {
+            return state.isLoggedIn;
+        }
+    }
+});
+
+export default store;
