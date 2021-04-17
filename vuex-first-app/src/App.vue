f076d3a03acc58c73ffe3b56417ed6d7c9c76175
mohamed085 <mohamed085555@gmail.com>
2021-04-18 00:25:03 +0200

    Add auth

--- vuex-first-app/src/App.vue
+++ vuex-first-app/src/App.vue
@@ -1,33 +1,44 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="isAuth">
     <the-counter></the-counter>
     <favorite-value></favorite-value>
-    <button @click="addOne">Add 1</button>
+    <button @click="addOne">Add 10</button>
     <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth></user-auth>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
-import TheCounter from "./components/TheCounter";
-import ChangeCounter from "./components/ChangeCounter";
-import FavoriteValue from "./components/FavoriteValue";
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+import UserAuth from './components/UserAuth.vue';
 
 export default {
   components: {
-    FavoriteValue,
-    ChangeCounter,
-    TheCounter,
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth,
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    },
   },
   methods: {
     addOne() {
-      this.$store.commit({
+      // this.$store.dispatch('increase', { value: 10 });
+      this.$store.dispatch({
         type: 'increase',
-        value: 10
+        value: 10,
       });
-    }
-  }
+    },
+  },
 };
 </script>
 
